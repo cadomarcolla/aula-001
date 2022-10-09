b251d54b2a516f4bef8d6a1f233171d68a6fbab1
cadomarcolla <ricardo_marcolla@yahoo.com.br>
2022-10-09 18:20:11 -0300

    Minha alteração na branch develop

--- hello.java
+++ hello.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello, World! - Alô Mundo Senai!"); 
+        System.out.println("Estou no develop"); 
     }
 }
