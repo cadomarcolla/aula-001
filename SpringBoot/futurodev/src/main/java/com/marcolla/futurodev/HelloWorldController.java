115b87d332cb77e430c67e759242c716b8b50e74
CadoMarcolla <ricardo_marcolla@yahoo.com.br>
2022-10-12 17:22:09 -0300

    Envio da primeira parte Projeto Spring Boot

---
+++ SpringBoot/futurodev/src/main/java/com/marcolla/futurodev/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.marcolla.futurodev;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/helloworld")
+    public String getHelloWorld(){
+        return "Hello World";
+    }
+}
